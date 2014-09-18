19219fb8bf8879312bf2e5a1d83c5289ad66fbc5
nandu <nandakishoremmn@gmail.com>
2014-09-18 19:33:23 +0530

    add template for localsettings

---
+++ iitmsatgss/localsettings.template.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iitmsat_gss',
+        'USER': 'root',
+        'PASSWORD': 'n4nbu',
+        'HOST': 'localhost',
+        # 'PORT': '5432',
+    }
+}
